0b3fa4954657aebe50481886440e245f196eb670
Jacob <jakesoto48@gmail.com>
2024-01-18 19:42:26 -0600

    Update Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,15 +1,23 @@
+import java.awt.Image;
 import java.util.Random;
+import javax.swing.ImageIcon;
 
 public class Dealer extends Player {
     Random random;
     private Deck cardDeck;
+    private Image backCardImage;
 
     public Dealer(int deckCount) {
         super();
         this.cardDeck = new Deck(deckCount);
         random = new Random();
+
+        backCardImage = new ImageIcon(getClass().getResource("./images/BACK.png")).getImage();
     }
 
+    public Image getBlankCard() {
+        return backCardImage;
+    }
 
     public void shuffleDeck() {
         cardDeck.shuffle();
