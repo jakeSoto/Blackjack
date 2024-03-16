0781f17bc8255a7c63f34a00297fc41ebd37c75f
Jacob <jakesoto48@gmail.com>
2024-03-16 04:25:16 -0500

    added gui functionality

--- Java/main.java
+++ Java/main.java
@@ -1,14 +1,50 @@
+import java.util.concurrent.TimeUnit;
+
 public class main {
-    public static void main(String[] args) {
-        /* 
-         * Initializing Blackjack gameManager generates the following:
-         *      Player object with an empty hand
-         *      Dealer object with an empty hand and shuffled card deck
-         */
+    static final String DEALER_TEXT = "Dealer Showing: ";
+    static final String PLAYER_TEXT = "Player Showing: ";
+
+    public static void main(String[] args) throws InterruptedException {
+        boolean playGame = true;
+        Window gameWindow = new Window("Blackjack");
         Blackjack gameManager = new Blackjack();
-        gameManager.dealHands();
-
+        Player player = gameManager.player;
+        Dealer dealer = gameManager.dealer;
         
+        while (playGame) {
+            playGame = false;
+            gameManager.dealHands();
+            Card dealerCard = dealer.getStartingHand();
 
+            //gameWindow.setDealerText(DEALER_TEXT + dealer.getStartingHand().getValue());
+            gameWindow.displayCard(dealerCard.getCardImage(), true);
+            gameWindow.displayCard(dealer.getBlankCard(), true);
+
+
+
+            //gameWindow.setPlayerText(PLAYER_TEXT + player.getHandTotal());
+            TimeUnit.SECONDS.sleep(2);
+
+            // if (player.hasBlackjack()) {
+            //     gameWindow.setPlayerText("Player has Blackjack!");
+            //     break;
+            // }
+            // else if (dealerCard.getValue() >= 10) {
+            //     gameWindow.setDealerText("Dealer checking for Blackjack.");
+            //     TimeUnit.SECONDS.sleep(2);
+
+            //     if (dealer.hasBlackjack()) {
+            //         gameWindow.setDealerText("Dealer has Blackjack, house wins!");
+            //         break;
+            //     }
+            //     else {
+            //         gameWindow.setDealerText(DEALER_TEXT + dealer.getStartingHand());
+            //     }
+            //}
+        
+        
+        
+        
+        }
     }
 }
