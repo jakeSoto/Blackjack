b5578b67dd65e0581d869b6f69ed776300680312
Jacob <jakesoto48@gmail.com>
2024-03-18 02:41:04 -0500

    added possibility for multiple players

--- src/blackjack/Blackjack.java
+++ src/blackjack/Blackjack.java
@@ -3,32 +3,56 @@ package blackjack;
 import java.awt.Image;
 
 public class Blackjack {
-	static int DECK_COUNT = 5;
+	private static int DECK_COUNT = 5;
 	
 	public Image CARD_BACK_IMAGE;
 	
-	protected Player player;
-	protected Dealer dealer;
+	private Player[] players;
+	private Dealer dealer;
 	
 	
 	public Blackjack() {
-		player = new Player();
+		/* Construct a game between a dealer and single player */
 		dealer = new Dealer(DECK_COUNT);
+		
+		players = new Player[1];
+		players[0] = new Player();
+		
 		dealer.shuffleDeck();
 		CARD_BACK_IMAGE = dealer.getCardBackImage();
 	}
 	
 	
-	public void dealHands() {
+	public Blackjack(int playersCount) {
+		/* Construct a game between a dealer and 1 to 3 players */
+		if (playersCount > 0 && playersCount < 4) {
+			dealer = new Dealer(DECK_COUNT);
+			this.players = new Player[playersCount];
+			
+			for (int i = 0; i < playersCount; i++) {
+				players[i] = new Player();
+			}
+		}
+		else {
+			throw new IllegalArgumentException("Must have between 1 and 3 Players!");
+		}
+
+		
+		dealer.shuffleDeck();
+		CARD_BACK_IMAGE = dealer.getCardBackImage();
+	}
+	
+	
+	public void dealStartingHands() {
 		Card tempCard = null;
 		
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 2; i++) {
 			tempCard = dealer.dealCard();
-			if (i % 2 == 0) {
-				player.addCard(tempCard);
-			}
-			else {
-				dealer.addCard(tempCard);
+			dealer.addCard(tempCard);
+			
+			for (int j = 0; j < this.players.length; j++) {
+				tempCard = dealer.dealCard();
+				players[j].addCard(tempCard);
 			}
 		}
 	}
@@ -37,28 +61,30 @@ public class Blackjack {
 	public Card getDealerCard() {
 		return dealer.getDealerCard();
 	}
+
 	
 	
-	
-	public Image[] getPlayerHandImages(boolean dealer) {
-		if (dealer) {
-			return getPlayerHandImages(this.dealer);
-		}
-		else {
-			return getPlayerHandImages(this.player);
-		}
-	}
-	
-	
-	
-	public Image[] getPlayerHandImages(Player player) {
-		Image[] handImages = new Image[player.hand.size()];
+	public Image[] getPlayerHandImages(int playerIndex) {
+		Player currentPlayer = players[playerIndex];
+		Image[] handImages = new Image[currentPlayer.hand.size()];
 		
-		for (int i = 0; i < player.hand.size(); i++) {
-			handImages[i] = player.hand.get(i).getCardImage();
+		for (int i = 0; i < currentPlayer.hand.size(); i++) {
+			handImages[i] = currentPlayer.hand.get(i).getCardImage();
 		}
 		
 		return handImages;
+	}
+	
+	
+	public int[] getHandTotals() {
+		int[] handTotals = new int[this.players.length + 1];
+		handTotals[0] = dealer.getHandTotal();
+		
+		for (int i = 0; i < this.players.length; i++) {
+			handTotals[i+1] = players[i].getHandTotal();
+		}
+		
+		return handTotals;
 	}
 	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,32 +4,97 @@ import gui.Window;
 import blackjack.Card;
 
 import java.awt.Image;
+import java.util.concurrent.TimeUnit;
 
 import blackjack.Blackjack;
 
 public class Main {
+	public static final int DEALER_IDENTIFIER = 0;
+	public static final int PLAYER_IDENTIFIER = 1;
+	public static final int PLAYER_COUNT = 1;
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		boolean playGame = false;
+		
 		Window gameWindow  = new Window("Blackjack");
-		Blackjack gameManager = new Blackjack();
+		Blackjack gameManager = new Blackjack(PLAYER_COUNT);
+		
 		
 		// Game Loop
 		do {
-			gameManager.dealHands();
-			Card dealerCard = gameManager.getDealerCard();
-			gameWindow.loadImage(gameManager.getDealerCard().getCardImage(), true);
+			gameManager.dealStartingHands();
 			
-			Image[] playerStartingHand = gameManager.getPlayerHandImages(false);
-			for (Image cardImage : playerStartingHand) {
-				gameWindow.loadImage(cardImage, false);
-			}
+			// Display starting hand images and totals to the GUI
+			loadStartingHands(gameManager, gameWindow);
+			TimeUnit.SECONDS.sleep(3);
 
-			gameWindow.loadImage(gameManager.CARD_BACK_IMAGE, true);
+			
+			
+			// Check for players with blackjack
+			int[] handTotals = gameManager.getHandTotals();
+			
+			for (int i = 1; i < handTotals.length; i++) {
+				if (handTotals[i] == 21) {
+					System.out.println("Player has Blackjack!");
+					break;
+				}
+			}
+			
+			// Assurance: checking for dealer blackjack 
+			if (gameManager.getDealerCard().isAce() || gameManager.getDealerCard().isFaceCard()) {
+				
+				System.out.println("Checking if dealer has Blackjack.");
+				TimeUnit.SECONDS.sleep(2);
+				
+				if (handTotals[DEALER_IDENTIFIER] == 21) {
+					System.out.println("Dealer has Blackjack!");
+					break;
+				}
+				else {
+					System.out.println("Dealer does not have Blackjack.");
+				}
+			}
+			
+			
+			
+			/* if player has doubles, ask if they would like to split 
+			 * otherwise ask player if they would like to hit or stay */
+			
 
 			
 		} while (playGame);
 		
+	}
+	
+	
+	public static void loadStartingHands(Blackjack gameManager, Window gameWindow) {
+		gameWindow.loadImage(gameManager.getDealerCard().getCardImage(), DEALER_IDENTIFIER);
+		gameWindow.loadImage(gameManager.CARD_BACK_IMAGE, DEALER_IDENTIFIER);
+		gameWindow.displayTotal(gameManager.getDealerCard().getValue(), DEALER_IDENTIFIER);
+		
+		int[] handTotals = gameManager.getHandTotals();
+		
+		for (int i = 0; i < PLAYER_COUNT; i++) {
+			Image[] playerStartingHand = gameManager.getPlayerHandImages(i);
+			
+			for (int j = 0; j < playerStartingHand.length; j++) {
+				gameWindow.loadImage(playerStartingHand[j], PLAYER_IDENTIFIER);
+			}
+			
+			gameWindow.displayTotal(handTotals[i+1], PLAYER_IDENTIFIER);
+		}
+	}
+	
+	
+	public static void displayPlayerTotals(Blackjack gameManager, Window gameWindow) {
+		int[] handTotals = gameManager.getHandTotals();
+		
+		gameWindow.displayTotal(handTotals[0], DEALER_IDENTIFIER);
+		
+		for (int i = 1; i < handTotals.length; i++) {
+			gameWindow.displayTotal(handTotals[i], PLAYER_IDENTIFIER);
+		}
 	}
 
 }
